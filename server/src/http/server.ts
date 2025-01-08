279a99fed1f959ece112e5593f0e04061441b80e
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-01-08 08:50:03 -0300

    backend finished

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -5,12 +5,17 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 
+import fastifyCors from '@fastify/cors'
 import { createCompletionRoute } from './routes/create-completion'
 import { createGoalRoute } from './routes/create-goal'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
