94f34720951538e7d33b72e68bcb519b21264bbe
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-01-10 12:51:26 -0300

    create empty goals, create goal and summary

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,21 +1,14 @@
-import { Plus } from 'lucide-react'
-import letsStartIllustration from './assets/lets-start-illustration.svg'
-import logo from './assets/logo-in-orbit.svg'
-import { Button } from './components/ui/button'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import { Dialog } from './components/ui/dialog'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in orbit" />
-      <img src={letsStartIllustration} alt="letsStartIllustration" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
-    </div>
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
+    </Dialog>
   )
 }
 
